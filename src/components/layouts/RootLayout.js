f657dc53d8ed7ddfb9084cd0cf3f18262525317b
shadmansaalim <shadmansaalim999@gmail.com>
2023-07-28 20:35:39 +0600

    Root Layout implemented for consisten layout system

---
+++ src/components/layouts/RootLayout.js
@@ -0,0 +1,16 @@
+import Navbar from "../shared/Navbar";
+import Footer from "../shared/Footer";
+
+const RootLayout = ({ children }) => {
+    return (
+        <div>
+            <Navbar />
+            <div className="min-h-screen">
+                {children}
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default RootLayout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import RootLayout from "@/components/layouts/RootLayout";
 
 const HomePage = () => {
   return (
     <div>
-
+      <h1 className="text-7xl">Home Page</h1>
     </div>
   );
 };
 
 export default HomePage;
+
+HomePage.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
