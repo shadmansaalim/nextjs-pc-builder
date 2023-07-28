69633a60d8b04ae7dbf1293f1db5510fa26f83bb
shadmansaalim <shadmansaalim999@gmail.com>
2023-07-28 22:42:00 +0600

    Navbar created according to requirements

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -8,36 +8,44 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
+                        <li><a>Home</a></li>
                         <li>
-                            <a>Parent</a>
+                            <a>Categories</a>
                             <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
+                                <li><a>CPU/Processor</a></li>
+                                <li><a>Motherboard</a></li>
+                                <li><a>RAM</a></li>
+                                <li><a>Power Supply Unit</a></li>
+                                <li><a>Storage Device</a></li>
+                                <li><a>Monitor</a></li>
+                                <li><a>Others</a></li>
                             </ul>
                         </li>
-                        <li><a>Item 3</a></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a className="btn btn-ghost normal-case text-xl">NextBuilder</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
+                    <li><a>Home</a></li>
                     <li tabIndex={0}>
                         <details>
-                            <summary>Parent</summary>
+                            <summary>Categories</summary>
                             <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
+                                <li><a>CPU/Processor</a></li>
+                                <li><a>Motherboard</a></li>
+                                <li><a>RAM</a></li>
+                                <li><a>Power Supply Unit</a></li>
+                                <li><a>Storage Device</a></li>
+                                <li><a>Monitor</a></li>
+                                <li><a>Others</a></li>
                             </ul>
                         </details>
                     </li>
-                    <li><a>Item 3</a></li>
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <a className="btn">PC Builder</a>
             </div>
         </div>
     );
